7fe8cc1f709a40318c0e8894c092f990052e82bf
Ainazik <ainazik.zhanarbekova@alatoo.edu.kg>
2026-02-15 22:26:02 +0600

    16 practice

---
+++ src/Main16.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main16 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                System.out.println("Indices: " + map.get(complement) + ", " + i);
+                return;
+            }
+            map.put(nums[i], i);
+        }
+
+        System.out.println("No solution found");
+    }
+}
